efbdde750f2f659385353a5a1cd96aeb23026c79
EzekielBanda <bsc-com-10-19@unima.ac.mw>
2021-12-29 16:37:20 -0800

    first commit on job portal api

---
+++ src/main/java/com/mwTech/Job_Portal_Api/JobPortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.mwTech.Job_Portal_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalApiApplication.class, args);
+	}
+
+}
